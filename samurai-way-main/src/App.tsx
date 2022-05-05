9d2020997e714c3416ec8f669ecea66b70dbd09a
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-05 22:38:44 +0300

    'fgb'

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src="https://mirpozitiva.ru/wp-content/uploads/2019/11/1472043884_02.jpg"/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+                <div>Message</div>
+            </nav>
+            <div className={'content'}>
+                <div>
+            <img src={"https://mirpozitiva.ru/wp-content/uploads/2019/11/1472043884_02.jpg"}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+
