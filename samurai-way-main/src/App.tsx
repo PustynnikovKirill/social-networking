1f0d7bfc05c8bce87a5a3d0b4144bab655aafac8
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-07 19:22:34 +0300

    'g'

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,50 +1,20 @@
 import React from 'react';
 import './App.css';
 
+
 export const App = () => {
     return (
-        <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img src="https://mirpozitiva.ru/wp-content/uploads/2019/11/1472043884_02.jpg"/>
+        <div className='app-wrapper'>
+            <header className = 'header'>
+                <img src='https://cdn.segodnya.ua/i/image_728x410/media/image/620/23c/d13/62023cd134531.jpg.webp'/>
             </header>
-            <nav className={'nav'}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-                <div>Message</div>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Profile</div>
+                <div>Profile</div>
             </nav>
-            <div className={'content'}>
-                <div>
-            <img src={"https://mirpozitiva.ru/wp-content/uploads/2019/11/1472043884_02.jpg"}/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My post
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        post 1
-                    </div>
-                    <div>
-                        post 2
-                    </div>
-                </div>
-            </div>
+            <div className= 'content'>Main content</div>
         </div>
     );
 }
