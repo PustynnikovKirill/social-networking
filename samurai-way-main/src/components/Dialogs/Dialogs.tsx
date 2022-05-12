e22d27c125cc0c25beb85eb3f83dae4556751032
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-13 00:10:28 +0300

    '21;

--- samurai-way-main/src/components/Dialogs/Dialogs.tsx
+++ samurai-way-main/src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import style from './Dialogs.module.css';
 
 export const Dialogs = () => {
   return(
-      <div >
-          Dialogs
+      <div className = {style.dialogs}>
+          <div className={style.dialogsItems}>
+              <div className={style.dialog + ' ' + style.active }>
+                 Dimych
+              </div>
+              <div className={style.dialog}>
+                  Sveta
+              </div>
+              <div className={style.dialog}>
+                  Andrey
+              </div>
+              <div className={style.dialog}>
+                  Sacha
+              </div>
+              <div className={style.dialog}>
+                  Valera
+              </div>
+          </div>
+          <div className = {style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How is your it-kamasutra!</div>
+                <div className={style.message}>Yo</div>
+          </div>
       </div>
   )
 }
