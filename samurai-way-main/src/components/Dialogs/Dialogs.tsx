dedd378662994f55491c1075833ec9ac3302e3b6
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-15 14:44:29 +0300

    'gbhdfh'

--- samurai-way-main/src/components/Dialogs/Dialogs.tsx
+++ samurai-way-main/src/components/Dialogs/Dialogs.tsx
@@ -2,30 +2,46 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Dialogs.module.css';
 
+export type PropsDialogItemType = {
+    name:string
+    id: string
+}
+
+export const DialogItem = (props:PropsDialogItemType) => {
+    let path = '/dialogs/' + props.id
+    return (
+    <div className={style.dialog + ' ' + style.active}>
+        <NavLink to = {path}>{props.name}</NavLink>
+    </div>
+    )
+}
+export type MessageType = {
+    message: string
+}
+
+const Message = (props:MessageType) => {
+    return (
+        <div className={style.message}>{props.message}</div>
+    )
+}
+
 export const Dialogs = () => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <div className={style.dialog + ' ' + style.active}>
-                    <NavLink to = '/dialogs/1'>Dimych</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to = '/dialogs/2'>Sveta</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to = '/dialogs/3'>Andrey</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to = '/dialogs/4'>Sacha</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink to = '/dialogs/5'>Valera</NavLink>
-                </div>
+                <DialogItem  name={'Dimych'} id={'1'}/>
+                <DialogItem  name={'Andrey'} id={'2'}/>
+                <DialogItem  name={'Sveta'} id={'3'}/>
+                <DialogItem  name={'Sasha'} id={'4'}/>
+                <DialogItem  name={'Viktor'} id={'5'}/>
+                <DialogItem  name={'Valera'} id={'6'}/>
             </div>
             <div className={style.messages}>
-                <div className={style.message}>Hi</div>
-                <div className={style.message}>How is your it-kamasutra!</div>
-                <div className={style.message}>Yo</div>
+                <Message message = {"Hi"}/>
+                <Message message = {"How is your it-kamasutra!"}/>
+                <Message message = {"Yo"}/>
+                <Message message = {"Yo"}/>
+                <Message message = {"Yo"}/>
             </div>
         </div>
     );
