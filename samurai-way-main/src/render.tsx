1c4731b409422850561b8c237542393e54a201f3
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-20 20:38:15 +0300

    'type'

---
+++ samurai-way-main/src/render.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App} from './App';
+import {state} from './redux/state'
+import {addPost} from './redux/state'
+
+
+export
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <App  state = {state} addPost={addPost}/>,
+        document.getElementById('root')
+    );
+}
+rerenderEntireTree();
