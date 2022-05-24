b68d8d9a886b12dede51aa9fa2261582fa0efa8e
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-24 21:17:48 +0300

    'observer'

--- samurai-way-main/src/index.tsx
+++ samurai-way-main/src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-import {state} from './redux/state'
-import {addPost} from './redux/state'
-import {rerenderEntireTree} from "./render";
+import {state, subscribe} from './redux/state'
+import ReactDOM from 'react-dom';
+import './index.css';
+import {addPost, RootStateType, updateNewPostText} from './redux/state'
+import {BrowserRouter} from "react-router-dom";
 
 
 
-rerenderEntireTree(state);
+export let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App  state = {state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+
+rerenderEntireTree();
+subscribe(rerenderEntireTree)
