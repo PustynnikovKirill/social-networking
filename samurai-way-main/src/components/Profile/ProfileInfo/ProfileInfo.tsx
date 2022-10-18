5a5f98ec752d73943cf53b952075782e610aa345
Pustynnikov Kirill <kiryuha911@yandex.by>
2022-10-18 11:58:05 +0300

    '71'

--- samurai-way-main/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ samurai-way-main/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './ProfileInfo.module.css'
 import {ProfileType} from "../ProfileContainer";
 import {Preloader} from "../../common/Preloader/Preloader";
+import {ProfileStatus} from "./ProfileStatus";
 
 type ProfileInfoType = {
     profile:ProfileType | null
@@ -15,12 +16,12 @@ export const ProfileInfo:React.FC<ProfileInfoType> = (props) => {
     return (
         <div>
             <div>
-                <img
-                    src='https://p4.wallpaperbetter.com/wallpaper/314/22/575/snowboarding-mountains-snow-wallpaper-preview.jpg'/>
+                {/*<img*/}
+                {/*    src='https://p4.wallpaperbetter.com/wallpaper/314/22/575/snowboarding-mountains-snow-wallpaper-preview.jpg'/>*/}
             </div>
             <div className={style.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava + description
+                <ProfileStatus status = {'Hello my friends'}/>
             </div>
         </div>
     )
