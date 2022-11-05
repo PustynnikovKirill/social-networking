99770e5f8b85a7a33ea77188c299992fc7b46ccb
Pustynnikov Kirill <kiryuha911@yandex.by>
2022-11-05 13:04:42 +0300

    '75'

--- samurai-way-main/src/redux/redux-store.ts
+++ samurai-way-main/src/redux/redux-store.ts
@@ -5,6 +5,7 @@ import {sidebarReducer} from "./sidebar.reducer";
 import {usersReducer} from "./users.reducer";
 import {authReducer} from "./auth-reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 let reducer = combineReducers({
     profilePage:profileReducer,
@@ -12,7 +13,7 @@ let reducer = combineReducers({
     sidebarPage:sidebarReducer,
     usersPages: usersReducer,
     auth:authReducer,
-
+    form:formReducer
 })
 
 export let store = createStore(reducer,applyMiddleware(thunk))
