34e6d253a632894834e0cfa971929aab4b50fc29
Pustynnikov Kirill <kiryuha911@yandex.by>
2023-02-08 14:18:20 +0300

    '99'

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import {Nav} from "./components/Nav/Nav";
-import {BrowserRouter,HashRouter, Route, withRouter} from "react-router-dom";
-import {AppRootStateType, RootStoreType, store} from "./redux/redux-store";
+import {BrowserRouter, Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {AppRootStateType, store} from "./redux/redux-store";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -16,7 +16,7 @@ import {Preloader} from "./components/common/Preloader/Preloader";
 
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer')
-    .then(({DialogsContainer})=>{
+    .then(({DialogsContainer}) => {
         return {
             default: DialogsContainer
         }
@@ -47,23 +47,31 @@ class App extends React.Component<MapStateToProps & MapDispatchToPropsType> {
                     <HeaderContainer/>
                     <Nav/>
                     <div className='app-wrapper-content'>
-                        <Route path='/dialogs' render={() => (
-                            <WithAuthRedirect>
-                                <React.Suspense fallback={<div>Loading...</div>}>
-                                    <DialogsContainer/>
-                                </React.Suspense>
-                            </WithAuthRedirect>
-                        )}/>
-                        <Route path='/profile/:userId?' render={() =>
-                            <WithAuthRedirect>
-                                <ProfileContainer/>
-                            </WithAuthRedirect>}/>
+                        <Switch>
+                            <Route exact path='/' render={() => (
+                                <WithAuthRedirect>
+                                    <ProfileContainer/>
+                                </WithAuthRedirect>
+                            )}/>
+                            <Route path='/dialogs' render={() => (
+                                <WithAuthRedirect>
+                                    <React.Suspense fallback={<div>Loading...</div>}>
+                                        <DialogsContainer/>
+                                    </React.Suspense>
+                                </WithAuthRedirect>
+                            )}/>
+                            <Route path='/profile/:userId?' render={() =>
+                                <WithAuthRedirect>
+                                    <ProfileContainer/>
+                                </WithAuthRedirect>}/>
 
-                        <Route path='/users' render={() =>
-                            <WithAuthRedirect>
-                                <UsersContainer/>
-                            </WithAuthRedirect>}/>
-                        <Route path='/login' render={() => <Login/>}/>
+                            <Route path='/users' render={() =>
+                                <WithAuthRedirect>
+                                    <UsersContainer/>
+                                </WithAuthRedirect>}/>
+                            <Route path='/login' render={() => <Login/>}/>
+                            {/*<Route path='*' render={() => <div>404 NOT FOUND</div>}/>*/}
+                        </Switch>
                     </div>
                 </div>
             </BrowserRouter>
@@ -77,11 +85,11 @@ const mapStateToProps = (state: AppRootStateType) => {
 let AppContainer = compose<React.FC>(withRouter, connect(mapStateToProps, {initializeApp}))(App)
 
 const SamuraiJSApp = (props: any) => {
-    return <HashRouter>
+    return <BrowserRouter>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 }
 
 export default SamuraiJSApp;
